c93557fe2f756860d81be3c9325b480712703062
Dan Nuttall <pixelblend@gmail.com>
2014-11-18 15:09:17 +0000

    Add zip output with full logs (messy, inline)

--- debug.go
+++ debug.go
@@ -1,34 +1,96 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/radiodan/debug/debug"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	port := parseFlags()
-	http.HandleFunc("/", debugResponse)
-	//http.HandleFunc("/download", downloadResponse)
+	http.HandleFunc("/view", viewResponse)
+	http.HandleFunc("/download", downloadResponse)
 
+	port := parseFlags()
 	log.Printf("Debug server running on http://127.0.0.1:%d", port)
 	http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), nil)
 }
 
-func debugResponse(w http.ResponseWriter, req *http.Request) {
+func viewResponse(w http.ResponseWriter, req *http.Request) {
 	d := debug.Build()
 
-	jsonReponse, err := json.Marshal(d)
+	jsonResponse, err := json.Marshal(d)
 	if err != nil {
-		msg := "Cannot marshal JSON"
-		log.Fatalf("%s: %s", msg, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonReponse)
+	w.Write(jsonResponse)
+}
+
+func downloadResponse(w http.ResponseWriter, req *http.Request) {
+	d := debug.Build()
+	jsonResponse, err := json.Marshal(d)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create zip bufer
+	buf := new(bytes.Buffer)
+
+	// Create zip archive.
+	zw := zip.NewWriter(buf)
+	f, err := zw.Create("debug.json")
+	f.Write([]byte(jsonResponse))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for _, app := range d.Applications {
+		filePath := app.LogFile()
+		fileName := filePath
+
+		file, err := os.Open(filePath)
+		defer file.Close()
+
+		if err != nil {
+			log.Println("[!] " + err.Error())
+			continue
+		}
+
+		zipWriter, err := zw.Create(fileName)
+
+		if err != nil {
+			log.Println("[!] " + err.Error())
+			continue
+		}
+		_, err = io.Copy(zipWriter, file)
+
+		if err != nil {
+			log.Println("[!] " + err.Error())
+			continue
+		}
+	}
+
+	err = zw.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/x-gzip")
+	w.Header().Set("Content-Disposition", "inline; filename=\"debug.zip\"")
+	w.Write(buf.Bytes())
 }
 
 func parseFlags() (port int) {
